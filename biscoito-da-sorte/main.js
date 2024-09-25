f6cca351b08e2ff7fbb468e527874b1327aa12b7
grpanisso <122941324+grpanisso@users.noreply.github.com>
2024-09-25 14:42:13 -0300

    Alteração no arquivo main.js

--- biscoito-da-sorte/main.js
+++ biscoito-da-sorte/main.js
@@ -1,14 +1,17 @@
+
 const screen1 = document.querySelector('.screen1');
 const screen2 = document.querySelector('.screen2');
-const biscoito = document.getElementById('imgbiscoitodasorte');
-const button = document.querySelector('button');
+const biscoitoButton = document.getElementById('imgbiscoitodasorte');
+const outroBiscoitoButton = document.querySelector('.screen2 button');
 
-biscoito.addEventListener('click', () => {
-    screen1.classList.add('hide'); 
-    screen2.classList.remove('hide'); 
+
+biscoitoButton.addEventListener('click', function() {
+    screen1.classList.add('hide');  // Esconde screen1
+    screen2.classList.remove('hide');  // Mostra screen2
 });
 
-button.addEventListener('click', () => {
-    screen1.classList.remove('hide'); 
-    screen2.classList.add('hide');
+
+outroBiscoitoButton.addEventListener('click', function() {
+    screen2.classList.add('hide');  // Esconde screen2
+    screen1.classList.remove('hide');  // Mostra screen1
 });
